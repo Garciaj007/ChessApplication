3914f06427ad3089180ee0473fad1cb2532c48c9
Juriel Garcia <jurielgarcia2010@gmail.com>
2018-10-11 22:20:43 -0400

    ChessApplication
    Creating a chess game in Java

---
+++ src/chessapplication/ChessApplication.java
@@ -0,0 +1,14 @@
+package chessapplication;
+
+/**
+ * @author Juriel Garcia, Kevin [LastName], Violet[LastName]
+ * @version 1.0.0
+ */
+public class ChessApplication {
+
+    public static void main(String[] args) {
+        Game g = new Game();
+        g.Run();
+    }
+    
+}
---
+++ src/chessapplication/Game.java
@@ -0,0 +1,13 @@
+package chessapplication;
+
+public class Game {
+    //Game loop Here
+    public void Run(){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                System.out.print(j);
+            }
+            System.out.println(i);
+        }
+    }
+}
