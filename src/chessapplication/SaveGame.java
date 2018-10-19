c28e2968a23739940c1b9523cb30afcd770ef7c4
Kevin Tang <tang-k@outlook.com>
2018-10-19 17:18:53 -0400

    1.0.8 Added Save and Load classes, menu logic in Game

---
+++ src/chessapplication/SaveGame.java
@@ -0,0 +1,17 @@
+package chessapplication;
+
+import java.util.*;
+/**
+ *
+ * @author kevin
+ */
+public class SaveGame {
+    //This class outputs data to a file, most likely a csv file
+    //Can overwrite file
+    SaveGame(){
+        //Calls upon the individual pieces and the board, then writes out to the file
+        //Can store as an object first
+        
+    }
+
+}
